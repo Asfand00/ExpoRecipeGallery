e177d549f4c97d1b9727f3e3c3a20cfac089a915
Asfandyar Tanwer <91708459+Asfand00@users.noreply.github.com>
2024-04-26 22:52:03 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Gallery from './Gallery'; // Adjust path as necessary
+import Gallery from './Gallery'; 
 
 export default function App() {
     return (
